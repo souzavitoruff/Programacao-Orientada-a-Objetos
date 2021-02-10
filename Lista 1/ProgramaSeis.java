b9721f6894c557f550b5b14c9be6c8d37869ee37
Vitor Souza <souzavitor@id.uff.br>
2021-02-10 20:21:09 -0300

    Correção no exercício 6

--- Lista 1/ProgramaSeis.java
+++ Lista 1/ProgramaSeis.java
@@ -1,17 +1,25 @@
 public class ProgramaSeis {
     public static void main(String[] args){
-        int termoUm = 1;
+        int termoUm = 0;
         int termoDois = 1;
-        int termoTres = 1;
-
-        System.out.println(0);
-        System.out.println(1);
+        int termoTres = 0;
 
         while(termoTres <= 100){
+            if(termoTres == 1){
+                System.out.println(termoTres);
+            }
+            
             System.out.println(termoTres);
             termoTres = termoUm + termoDois;
             termoUm = termoDois;
             termoDois = termoTres;
+            
+            if(termoUm + termoDois > 100){
+                System.out.println(termoTres);
+                System.out.println(termoUm + termoDois);
+                break;
+            }
+
         }
 
     }
